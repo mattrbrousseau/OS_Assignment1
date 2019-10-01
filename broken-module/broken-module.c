cef3a5800b20fa718da6eab49ac40ec625bd09a7
cs469 <cs469@cs469.cs.toronto.edu>
2019-10-01 11:45:15 -0400

    broken module

---
+++ broken-module/broken-module.c
@@ -0,0 +1,24 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_DESCRIPTION("Testing null pointer exceptions");
+MODULE_AUTHOR("Matthew Brousseau - brouss16");
+MODULE_LICENSE("GPL");
+
+static int __init simple_init(void) {
+	int *x1 = NULL;
+	int x2 = *x1;
+	//Lyrics from Comfortably Numb by Pink Floyd
+	printk(KERN_INFO "Hello? Is there anybody in there? Just nod if you can hear me\n");
+	return 0;
+}
+
+static void __exit simple_exit(void) {
+
+	//Lyrics from Candle in the Wind by Elton John
+	printk(KERN_INFO "Goodbye Norma Jean, though I never knew you at all.\n");
+}
+
+module_init(simple_init)
+module_exit(simple_exit)
